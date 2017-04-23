973dba9f9d97aaa4891e9db4cc6b49547189dc3b
Jelle van der Waa <jelle@vdwaa.nl>
2017-04-23 18:41:04 +0200

    Use extension settings instead of hardcoded url

--- content.js
+++ content.js
@@ -1,10 +1,19 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if ( request.message === "clicked_browser_action" ) {
-      fetch("https://vps.vdwaa.nl/urlgrab", {
-        method: 'POST',
-        headers: {'Content-Type':'application/json'},
-        body: JSON.stringify({url: window.location.href, title: document.title})
+      chrome.storage.sync.get({
+        push_url: '',
+        secret_token: ''
+      }, function(items) {
+        fetch(items.push_url, {
+          method: 'POST',
+          headers: {'Content-Type':'application/json'},
+          body: JSON.stringify({
+            url: window.location.href,
+            title: document.title,
+            token: items.secret_token
+          })
+        });
       });
     }
   }
